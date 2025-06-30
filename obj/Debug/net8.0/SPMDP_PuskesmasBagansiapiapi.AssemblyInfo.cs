799311994c9de7ef0c0bcffc124aeff04b651f52
Arifi <arifiramadhan26@gmail.com>
2025-06-30 09:07:55 +0700

    progres

--- obj/Debug/net8.0/SPMDP_PuskesmasBagansiapiapi.AssemblyInfo.cs
+++ obj/Debug/net8.0/SPMDP_PuskesmasBagansiapiapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SPMDP_PuskesmasBagansiapiapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b826e7f5147c1b533c9211bf1d44ed60d0a79825")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a00164b3d504b31a1bfd09126d76ff28c94bfb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SPMDP_PuskesmasBagansiapiapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SPMDP_PuskesmasBagansiapiapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
