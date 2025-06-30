517553699696acb66afac6b819b128945f68ddfe
mohd22si <mohd22si@mahasiswa.pcr.ac.id>
2025-06-30 08:47:44 +0700

    first

--- obj/Debug/net8.0/SPMDP_PuskesmasBagansiapiapi.AssemblyInfo.cs
+++ obj/Debug/net8.0/SPMDP_PuskesmasBagansiapiapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SPMDP_PuskesmasBagansiapiapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4eb419242c51b9f297b5e264c70ab8e2f883a88")]
 [assembly: System.Reflection.AssemblyProductAttribute("SPMDP_PuskesmasBagansiapiapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SPMDP_PuskesmasBagansiapiapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
